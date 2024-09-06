6e4a7fb18d06a4403a661c4ad753893e20ec7c13
alvenstevanus <alvenstevanuss@gmail.com>
2024-09-06 16:07:30 +0700

    Praktikum 5B sampai 5E Penggunaan Require dan Include

--- empat.php
+++ empat.php
@@ -2,4 +2,3 @@
 require_once "Author.php";
 require_once "Book.php";
 require_once "Publisher.php";
-    
--- keenam.php
+++ keenam.php
@@ -1,7 +1,6 @@
 <?php
 
-include_once "kelima.php";
-include_once "kelima.php";
-include_once "kelima.php";
-include_once "kelima.php";
+require_once "kelima.php";
+require_once "kelima.php";
+require_once "ketujuh.php";
 echo "ini file keenam.php\n";
