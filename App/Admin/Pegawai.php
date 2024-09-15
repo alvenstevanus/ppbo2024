1ca70419c7b0e9501af484f193663d52fc7649e2
alvenstevanus <alvenstevanuss@gmail.com>
2024-09-16 03:42:12 +0700

    Praktikum 6E - Menggunakan Pewarisan

--- App/Admin/Pegawai.php
+++ App/Admin/Pegawai.php
@@ -32,29 +32,3 @@ class Pegawai {
         $this->no_hp = $no_hp;
     }
 }
-
-class Dosen extends Pegawai {
-    public string $nidn;
-
-    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, string $nidn) {
-        parent::__construct($nip, $nama, $no_hp, $alamat);
-        $this->nidn = $nidn;
-    }
-
-    public function mengajar(): void {
-        echo $this->nama . " sedang mengajar perkuliahan";
-    }
-}
-
-class TenagaKependidikan extends Pegawai {
-    public int $gaji_pokok;
-
-    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, int $gaji_pokok) {
-        parent::__construct($nip, $nama, $no_hp, $alamat);
-        $this->gaji_pokok = $gaji_pokok;
-    }
-
-    public function cuti(): void {
-        echo $this->nama . " sedang cuti";
-    }
-}
