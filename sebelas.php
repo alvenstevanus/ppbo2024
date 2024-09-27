6f180bc1623b1dfdb4de13e8ebb7458859630cf2
alvenstevanus <alvenstevanuss@gmail.com>
2024-09-27 18:48:41 +0700

    z

--- sebelas.php
+++ sebelas.php
@@ -1,8 +1,8 @@
-<?php
+    <?php
 
-require_once 'vendor/autoload.php';
+    require_once 'vendor/autoload.php';
 
-use App\Admin\Dosen;
+    use App\Admin\Dosen;
 
-$dian=new Dosen(198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405");
-$dian->mengajar();
+    $dian=new Dosen(198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405");
+    $dian->mengajar();
